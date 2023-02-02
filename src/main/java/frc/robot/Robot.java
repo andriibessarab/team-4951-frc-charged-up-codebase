ed34c03f714dfe3adbf32d890bce3370cea83387
Andrii Bessarab <andriibessarab@gmail.com>
2023-02-02 15:31:40 -0500

    Implement mecanum drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,9 @@
 package frc.robot;
 
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Timer;
@@ -13,26 +14,24 @@ public class Robot extends TimedRobot {
   private final XboxController m_controller = new XboxController(0);
 
   // Motors
-  private final Victor frontLeftMotor = new Victor(2);
-  private final Victor frontRightMotor = new Victor(3);
   private final Victor rearLeftMotor = new Victor(0);
   private final Victor rearRightMotor = new Victor(1);
+  private final Victor frontLeftMotor = new Victor(2);
+  private final Victor frontRightMotor = new Victor(3);
 
-  // Mecanum Drive
-  public final MecanumDrive m_drive = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+  // Group of 4 motors
+  private final MotorControllerGroup motorGroup = new MotorControllerGroup(rearLeftMotor, rearRightMotor, frontLeftMotor, frontRightMotor);
 
   // Timer
   private final Timer m_timer = new Timer();
 
+  // State variables
+  private boolean motorsOn = true;
+
   @Override
   public void robotInit() {
-    // Disable robot safety
-    m_drive.setSafetyEnabled(false);
-    m_drive.feed();
-    //rearLeftMotor.setInverted(true);
+    // Invert neccessary motors
     rearRightMotor.setInverted(true);
-    //frontLeftMotor.setInverted(true);
-
   }
 
 
@@ -55,47 +54,42 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // Disable robot safety
-    m_drive.setSafetyEnabled(false);
-    m_drive.feed();
   }
 
 
   @Override
   public void teleopPeriodic() {
-    // Emergency stop driving - X Button
+    // Turn motors on/off - X Button
     if(m_controller.getXButton()) {
-      m_drive.stopMotor();
+      if (motorsOn)
+      {
+        motorGroup.stopMotor();
+        motorsOn = false;
+      } else {
+        motorGroup.set(1);
+        motorsOn = true;
+      }
     }
 
+    // Retrive axis from controller
     double y = -m_controller.getLeftY();
-    double x = m_controller.getLeftX() * 1.1;
+    double x = m_controller.getLeftX() * 1.1; // Counteract imperfect strafing
     double rx = m_controller.getRightX();
 
+    // Calculate denominator
     double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
+    // Calculate motor power
+    double rearLeftPower = (y - x + rx) / denominator;
     double frontLeftPower = (y + x + rx) / denominator;
-    double backLeftPower = (y - x + rx) / denominator;
     double frontRightPower = (y - x - rx) / denominator;
-    double backRightPower = (y + x - rx) / denominator;
+    double rearRightPower = (y + x - rx) / denominator;
 
-    if(frontLeftPower>0.5){
-      frontLeftPower=0.5;
-    }
-    if(backLeftPower>0.5){
-      backLeftPower=0.5;
-    }
-    if(frontRightPower>0.5){
-      frontRightPower=0.5;
-    }
-    if(backRightPower>0.5){
-      backRightPower=0.5;
-    }
-
+    // Send power to motors
+    rearLeftMotor.set(rearLeftPower);
+    rearRightMotor.set(rearRightPower);
     frontLeftMotor.set(frontLeftPower);
-    rearLeftMotor.set(backLeftPower);
     frontRightMotor.set(frontRightPower);
-    rearRightMotor.set(backRightPower);
   }
 
 
