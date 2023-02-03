52b7bd39e4d3fe6fa9ca5ef3187d6f544e1afd0a
Andrii Bessarab <andriibessarab@gmail.com>
2023-02-03 09:35:07 -0500

    Store limelight values and switch y to rotY in mecanum drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,31 @@
 package frc.robot;
 
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Timer;
 
 
 public class Robot extends TimedRobot {
+
   // Xbox Controller
   private final XboxController m_controller = new XboxController(0);
 
   // Gyro
   private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+  // Limelight
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry limelightTv = table.getEntry("tv");
+  NetworkTableEntry limelightTx = table.getEntry("tx"); 
+  NetworkTableEntry limelightTy = table.getEntry("ty");
+  NetworkTableEntry limelightTa = table.getEntry("ta");
 
   // Motors
   private final Victor rearLeftMotor = new Victor(0);
@@ -39,7 +51,19 @@ public class Robot extends TimedRobot {
 
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // Read limelight values periodically
+    boolean limelightHasTarget = limelightTv.getBoolean(false);     // Whether the limelight has any valid targets (0 or 1)
+    double limelightHorizontalOffset = limelightTx.getDouble(0.0);  // Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+    double limelightVerticalOffset = limelightTy.getDouble(0.0);    // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+    double limelightTargetArea = limelightTa.getDouble(0.0);        // Target Area (0% of image to 100% of image)  
+
+    // DEBUG - Update smart dashboard periodically
+    SmartDashboard.putBoolean("Limelight Has Target", limelightHasTarget);
+    SmartDashboard.putNumber("Limelight Horizontal Offset", limelightHorizontalOffset);
+    SmartDashboard.putNumber("Limelight Vertical Offset", limelightVerticalOffset);
+    SmartDashboard.putNumber("Limelight Target Area", limelightTargetArea);
+  }
 
 
   @Override
@@ -75,16 +99,22 @@ public class Robot extends TimedRobot {
     double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
     // Calculate motor power
-    double rearLeftPower = (y - rotX + rx) / denominator;
-    double frontLeftPower = (y + rotX + rx) / denominator;
-    double frontRightPower = (y - rotX - rx) / denominator;
-    double rearRightPower = (y + rotX - rx) / denominator;
+    double rearLeftPower = (rotY - rotX + rx) / denominator;
+    double frontLeftPower = (rotY + rotX + rx) / denominator;
+    double frontRightPower = (rotY - rotX - rx) / denominator;
+    double rearRightPower = (rotY + rotX - rx) / denominator;
 
     // Send power to motors
     rearLeftMotor.set(rearLeftPower * SpeedMultiplier);
     rearRightMotor.set(rearRightPower * SpeedMultiplier);
     frontLeftMotor.set(frontLeftPower * SpeedMultiplier);
     frontRightMotor.set(frontRightPower * SpeedMultiplier);
+
+    // DEBUG - Update smart dashboard periodically
+    SmartDashboard.putNumber("Rear Left Motor Power", rearLeftPower);
+    SmartDashboard.putNumber("Rear Right Motor Power", rearRightPower);
+    SmartDashboard.putNumber("Front Left Motor Power", frontLeftPower);
+    SmartDashboard.putNumber("Front Right Motor Power", frontRightPower);
   }
 
 
