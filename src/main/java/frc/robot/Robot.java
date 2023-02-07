6c1c2683136d6be85fb0b3a9b4a1c891d07fdd04
Andrii Bessarab <andriibessarab@gmail.com>
2023-02-07 09:52:24 -0500

    Convert gyroscope angle to radians

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,25 +1,32 @@
 package frc.robot;
 
 
+import java.io.IOException;
+import java.nio.file.Path;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.Timer;
 
 
 public class Robot extends TimedRobot {
-
+  // This will fix all your problems!!!
+  public static final String Jason = "Jason wrote this line of code";
+  
   // Xbox Controller
-  private final XboxController m_controller = new XboxController(0);
-
+  private final XboxController mController = new XboxController(0);
+  
   // Gyro
-  private final Gyro gyro = new ADXRS450_Gyro();
+  private final ADIS16470_IMU gyro = new ADIS16470_IMU();
 
   // Limelight
   private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -34,11 +41,17 @@ public class Robot extends TimedRobot {
   private final Victor frontLeftMotor = new Victor(2);
   private final Victor frontRightMotor = new Victor(3);
 
+  // PathWeaver
+  // private final String trajectoryJSON = "paths/Test.wpilib.json";
+  // pruvate Trajectory trajectory = new Trajectory();
+
   // Timer
-  private final Timer m_timer = new Timer();
+  private final Timer mTimer = new Timer();
 
   // Variables
-  private double SpeedMultiplier = 1; // 0 < motorSpeed <= 1 Change this to reduce overall speed of motorls
+  private double SpeedMultiplier = 0.5; // 0 < motorSpeed <= 1 Change this to reduce overall speed of motorls
+  //private double angle = 0; 
+
 
   @Override
   public void robotInit() {
@@ -47,6 +60,14 @@ public class Robot extends TimedRobot {
 
     // Calibrate gyroscope
     gyro.calibrate();
+
+    // Pathweaver retrieve path(s)
+    /*try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    }*/
   }
 
 
@@ -69,8 +90,8 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     // Reset tier before starting autonomous
-    m_timer.reset();
-    m_timer.start();
+    mTimer.reset();
+    mTimer.start();
   }
 
 
@@ -85,24 +106,54 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     // Retrive axis from controller
-    double y = -m_controller.getLeftY();
-    double x = m_controller.getLeftX() * 1.1; // Counteract imperfect strafing
-    double rx = m_controller.getRightX();
+    double y = mController.getLeftY();
+    double x = mController.getLeftX() * 1.1; // Counteract imperfect strafing
+    double rx = mController.getRightX();
 
-    double botHeading = gyro.getAngle();
+    // Calculate denominator
+    double denominator;
+
+    double rearLeftPower=0;
+    double frontLeftPower=0;
+    double rearRightPower=0;
+    double frontRightPower=0;
+
+    double botHeading = (Math.PI / 180) * gyro.getAngle();
 
     // Rotate the movement direction counter to the bot's rotation
     double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
     double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+    denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+    rearLeftPower = (rotY - rotX + rx) / denominator;
+    frontLeftPower = (rotY + rotX + rx) / denominator;
+    frontRightPower = (rotY - rotX - rx) / denominator;
+    rearRightPower = (rotY + rotX - rx) / denominator;
 
-    // Calculate denominator
-    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-    // Calculate motor power
-    double rearLeftPower = (rotY - rotX + rx) / denominator;
-    double frontLeftPower = (rotY + rotX + rx) / denominator;
-    double frontRightPower = (rotY - rotX - rx) / denominator;
-    double rearRightPower = (rotY + rotX - rx) / denominator;
+    /*if(rx<0.2&&rx>0.2){
+      // Calculate motor power
+
+      double botHeading = (Math.PI / 180) * gyro.getAngle();
+
+      // Rotate the movement direction counter to the bot's rotation
+      double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+      double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+      denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+      rearLeftPower = (rotY - rotX + rx) / denominator;
+      frontLeftPower = (rotY + rotX + rx) / denominator;
+      frontRightPower = (rotY - rotX - rx) / denominator;
+      rearRightPower = (rotY + rotX - rx) / denominator;
+    } else {
+      // Calculate motor power
+      denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+      rearLeftPower = (y - x + rx) / denominator;
+      frontLeftPower = (y + x + rx) / denominator;
+      frontRightPower = (y - x - rx) / denominator;
+      rearRightPower = (y + x - rx) / denominator;
+      gyro.reset();
+    }*/
+
+
 
     // Send power to motors
     rearLeftMotor.set(rearLeftPower * SpeedMultiplier);
@@ -111,6 +162,7 @@ public class Robot extends TimedRobot {
     frontRightMotor.set(frontRightPower * SpeedMultiplier);
 
     // DEBUG - Update smart dashboard periodically
+    SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
     SmartDashboard.putNumber("Rear Left Motor Power", rearLeftPower);
     SmartDashboard.putNumber("Rear Right Motor Power", rearRightPower);
     SmartDashboard.putNumber("Front Left Motor Power", frontLeftPower);
