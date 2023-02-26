8eae4a7d867c87ba5f0115c6ba85612cae0cafed
Andrii Bessarab <andriibessarab@gmail.com>
2023-02-26 11:43:17 -0500

    Move controller instintation to Robot,java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,9 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.utils.Controller;
-
 
 /**
  *This class holds all the inputs and constants used in the robot's code.
@@ -22,14 +18,4 @@ public final class RobotMap {
 
 	/** The PWM pin number of the Limelight sensor.*/
 	public static int LIMELIGHT_PWM_PIN; // undefined
-
-	/** The threshold value of the controller movement below which the movement is ignored. */
-	public static final double CONTROLLER_THRESHOLD = 0.2;
-
-	/** The Xbox controller objects used for driving the robot. */
-	public final static Controller mController1 = new Controller(XBOX_CONTROLLER_1_ID);
-    public final static Controller mController2 = new Controller(XBOX_CONTROLLER_2_ID);
-
-	SendableChooser<Command> trajectories = new SendableChooser();
-
 }
