20abd0b487d57a753054dcd8481bf77db9ce2942
Andrii Bessarab <andriibessarab@gmail.com>
2023-11-03 22:15:23 -0400

    Limit pivot go to command to run for two seconds max

--- src/main/java/frc/robot/commands/CmdHybridManip_PivotGoToPosition.java
+++ src/main/java/frc/robot/commands/CmdHybridManip_PivotGoToPosition.java
@@ -1,11 +1,14 @@
 package frc.robot.Commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.Subsys_Pivot;
 
 public class CmdHybridManip_PivotGoToPosition extends CommandBase {
     private final Subsys_Pivot m_pivot;
     private final double m_position;
+    Timer m_timer = new Timer();
+
 
     public CmdHybridManip_PivotGoToPosition(Subsys_Pivot pivot, double position) {
         m_pivot = pivot;
@@ -21,10 +24,7 @@ public class CmdHybridManip_PivotGoToPosition extends CommandBase {
     @Override
     public boolean isFinished() {
         double current = m_pivot.getPosition();
-        if (Math.abs(current-m_position) < 0.1) {
-            return true;
-        }
-        return false;
+        return Math.abs(current-m_position) < 0.1 || m_timer.get() > 2;
     }
 
     @Override
